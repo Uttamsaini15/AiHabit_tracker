29c5d92618371ba1649d9a0c12365b04fbb6c196
Uttam Saini <144931964+Uttamsaini15@users.noreply.github.com>
2026-08-01 20:08:10 +0530

    Update baseURL in axios configuration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
   const api = axios.create({
-    baseURL: "https://aihabit-tracker-backend.onrender.com",
+    baseURL: "https://aihabit-tracker-backend.onrender.com/api",
   });
 
   api.interceptors.request.use((config) => {
